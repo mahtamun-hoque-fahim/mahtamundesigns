a8da4542d1e4327b477c35325704b61bb212aa94
Mahtamun Hoque Fahim <fahim@mahtamun.dev>
2026-07-03 23:09:54 +0000

    Minimize card widths in Featured Projects grid on large screens

--- components/sections/featured-projects.tsx
+++ components/sections/featured-projects.tsx
@@ -99,10 +99,10 @@ export function FeaturedProjects() {
           </h2>
         </div>
 
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-          <ProjectCard project={featured} className="min-h-[420px] md:col-span-2" />
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:px-12 xl:px-24">
+          <ProjectCard project={featured} className="min-h-[280px] md:col-span-2" />
           {rest.map((project) => (
-            <ProjectCard key={project.title} project={project} className="min-h-[320px]" />
+            <ProjectCard key={project.title} project={project} className="min-h-[220px]" />
           ))}
         </div>
 

10b3b4579ea5bf2a25cc85770df0b0b8559cb055
Mahtamun Hoque Fahim <fahim@mahtamun.dev>
2026-07-04 00:13:35 +0000

    Increase featured project card heights by 10%

--- components/sections/featured-projects.tsx
+++ components/sections/featured-projects.tsx
@@ -121,9 +121,9 @@ export function FeaturedProjects() {
         </div>
 
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:px-12 xl:px-24">
-          <ProjectCard project={featured} className="min-h-[280px] md:col-span-2" />
+          <ProjectCard project={featured} className="min-h-[308px] md:col-span-2" />
           {rest.map((project) => (
-            <ProjectCard key={project.title} project={project} className="min-h-[220px]" />
+            <ProjectCard key={project.title} project={project} className="min-h-[242px]" />
           ))}
         </div>
 
